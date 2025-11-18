a4c8002a26bce6991fcc49689c996811edc14aac
junhyeog-cmd <junhyeoggang832@gmail.com>
2025-11-18 14:03:53 +0900

    Update 06_지하철 분석.py

--- pages/06_지하철 분석.py
+++ pages/06_지하철 분석.py
@@ -1,11 +1,16 @@
-import os
+import streamlit as st
 import pandas as pd
+import os
+
+st.set_page_config(page_title="서울 지하철 분석", layout="wide")
 
 @st.cache_data
 def load_data():
-    base_dir = os.path.dirname(__file__)            # pages 폴더 경로
+    base_dir = os.path.dirname(__file__)  # pages 폴더 경로
     csv_path = os.path.abspath(os.path.join(base_dir, "..", "subway.csv"))
-    
+
     df = pd.read_csv(csv_path, encoding="cp949")
     df["사용일자"] = df["사용일자"].astype(str)
     return df
+
+df = load_data()
